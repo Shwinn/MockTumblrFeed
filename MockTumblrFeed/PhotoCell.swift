0da5f2e4872d4b690fb74b4ff0f0f26ed1aea5e0
Shwinn <ashwingupta@Ashwins-iMac.local>
2017-02-04 21:58:10 -0800

    Finished Mock Tumblr Feed

---
+++ MockTumblrFeed/PhotoCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoCell.swift
+//  MockTumblrFeed
+//
+//  Created by Ashwin Gupta on 2/2/17.
+//  Copyright © 2017 Ashwin Gupta. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    @IBOutlet weak var pictureView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
